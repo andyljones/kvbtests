35426e3ad04cda365fd7b01ebdcc234587552b02
Andrew Jones <andyjones.ed@gmail.com>
2016-08-21 19:16:53 +0100

    Adds t stat calculation

--- scratch.py
+++ scratch.py
@@ -11,3 +11,24 @@ def sample_kvb(size):
     w1 = w[:, [-1]]
     bottom = 1./len(r)*((w - r[None, :]*w1)**2).sum(1, keepdims=True)
     return (w1/sp.sqrt(bottom))[:, 0]
+
+def critical_values(ps=[90, 95, 99], size=100000):
+    chunk_size = min(size, 10000)
+    n_chunks = int(size/chunk_size) + 1
+    samples = sp.hstack([sample_kvb(chunk_size) for _ in range(n_chunks)])[:size]
+    return pd.Series(sp.percentile(samples, ps), ps)
+    
+def t_stat(X, y):
+    T = y.shape[0]
+    beta = sp.linalg.solve(X.T.dot(X), X.T.dot(y))
+    u = y - X.dot(beta)
+    
+    S = sp.cumsum(X*u[:, None], 0)
+    C = 1./T**2 * (S[:, :, None]*S[:, None, :]).sum(0)
+    
+    Q = 1./T * (X[:, None, :]*X[:, :, None]).sum(0)
+    
+    B = sp.linalg.inv(Q).dot(C).dot(sp.linalg.inv(Q))
+    
+    sigmas = sp.sqrt(sp.diag(B)/T)
+    return beta/sigmas
