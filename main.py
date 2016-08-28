891bdf6f27f58acc706b2e3f89d63f9c17de74e0
Andrew Jones <andyjones.ed@gmail.com>
2016-08-28 10:08:27 +0100

    Fixes readme

--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 
 This module is based on the work from these papers:
 
-    - Kiefer, Vogelsang, Bunzel (2000) - Simple, Robust Testing of Regression Hypotheses
-    - Kiefer, Vogelsang (2002) - Heteroskedasticity-Autocorrelation Robust Standard Errors Using the Bartlett Kernel Without Truncation
-    - Abadir, Paruolo (2002) - Simple Robust Testing of Regression Hypotheses: A Comment
-    - Abadir, Paruolo (1997) - Two Mixed Normal Densities from Cointegration Analysis
+ - Kiefer, Vogelsang, Bunzel (2000) - Simple, Robust Testing of Regression Hypotheses
+ - Kiefer, Vogelsang (2002) - Heteroskedasticity-Autocorrelation Robust Standard Errors Using the Bartlett Kernel Without Truncation
+ - Abadir, Paruolo (2002) - Simple Robust Testing of Regression Hypotheses: A Comment
+ - Abadir, Paruolo (1997) - Two Mixed Normal Densities from Cointegration Analysis
 
 The main function of interest is :func:`t_stat`.
 
